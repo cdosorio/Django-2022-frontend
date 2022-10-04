1d82d418a73a0f613d9b42de5e9c871fa4c21e96
cdosorio <cdosorio@gmail.com>
2022-10-03 22:32:05 -0300

    Storing token in frontend

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+//  ------------------  Login ------------------
+let loginBtn = document.getElementById('login-btn');
+let logoutBtn = document.getElementById('logout-btn');
+let token = localStorage.getItem('token')
+
+if (token){
+  loginBtn.remove()
+}else{
+  logoutBtn.remove()
+}
+
+logoutBtn.addEventListener('click', (e)=> {
+  e.preventDefault();
+  localStorage.removeItem('token')
+  window.location = 'http://127.0.0.1:5500/login.html';
+})
+
+
+
+
+//  ------------------  List projects ------------------
 let baseUrl = "http://127.0.0.1:8000";
 let projectsUrl = baseUrl + "/api/projects/";
 
@@ -15,23 +36,22 @@ let buildProjects = (projects) => {
 
   for (let i = 0; i < projects.length; i++) {
     const project = projects[i];
-    //console.log(project)
 
     let projectCard = `
             <div class="project--card">
                 <img src="${baseUrl}/${project.featured_image}">
                 <div>
                     <div class="card--header">
-                        <h3>${project.title}</h3>      
+                        <h3>${project.title}</h3>
                         <strong class="vote--option" data-vote="up" data-project="${
                           project.id
                         }">&#43; </strong>
                         <strong class="vote--option" data-vote="down" data-project="${
                           project.id
                         }">&#8722; </strong>
-                    </div>                
+                    </div>
                 </div>
-                
+
                 <i>${project.vote_ratio}% Positive feedback </i>
                 <i>${project.description.substring(0, 150)} </i>
 
@@ -57,7 +77,6 @@ let addVoteEvents = () => {
 
       // Sending POST to API
       const url = `${projectsUrl}${project}/vote/`;
-      //console.log("URL: ", url);
 
       fetch(url, {
         method: "POST",
